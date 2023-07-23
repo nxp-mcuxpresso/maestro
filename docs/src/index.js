d557a7153947a2b1545176c2d40faa45f7e9be87
MCUSDK Jenkins CI <mcu_sdk_ci_jenkins@nxp.com>
2023-07-23 15:13:33 +0000

    Add release/2.14.0_major_rfp update
    - update cmake files
    - add additional lib and xml files

---
+++ docs/src/index.js
@@ -0,0 +1,29 @@
+var index =
+[
+    [ "Introduction", "index.html#autotoc_md1", null ],
+    [ "Supported examples", "index.html#autotoc_md2", [
+      [ "maestro_playback", "index.html#autotoc_md3", null ],
+      [ "maestro_record", "index.html#autotoc_md4", null ],
+      [ "maestro_usb_mic", "index.html#autotoc_md5", null ],
+      [ "maestro_usb_speaker", "index.html#autotoc_md6", null ],
+      [ "maestro_sync", "index.html#autotoc_md7", null ]
+    ] ],
+    [ "Example applications overview", "index.html#autotoc_md8", null ],
+    [ "Commands", "index.html#autotoc_md9", [
+      [ "version", "index.html#autotoc_md10", null ],
+      [ "file", "index.html#autotoc_md11", null ],
+      [ "record_mic", "index.html#autotoc_md12", null ],
+      [ "opus encode", "index.html#autotoc_md13", null ],
+      [ "usb_mic", "index.html#autotoc_md14", null ],
+      [ "usb_speaker", "index.html#autotoc_md15", null ],
+      [ "start", "index.html#autotoc_md16", null ]
+    ] ],
+    [ "Configuration options", "index.html#autotoc_md17", null ],
+    [ "Supported features", "index.html#autotoc_md18", [
+      [ "Codecs", "index.html#autotoc_md19", null ],
+      [ "VIT", "index.html#autotoc_md20", null ],
+      [ "VoiceSeeker", "index.html#autotoc_md21", null ],
+      [ "EAP", "index.html#autotoc_md22", null ],
+      [ "SSRC", "index.html#autotoc_md23", null ]
+    ] ]
+];
